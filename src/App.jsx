9cdf728876aa9fc8a5ed89c2a0e7d9b8e87c171e
Faizan Shaikh <faizan0957@gmail.com>
2025-10-25 20:57:12 +0530

    Testing: Pushing minimal App.jsx to debug blank page

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,16 @@
 // src/App.jsx
 
-import { Routes, Route } from 'react-router-dom';
+import React from 'react';
 import { Container } from 'react-bootstrap';
-
-// Import Layout Components
-import Header from './components/Header';
-import Footer from './components/Footer';
-
-// Import Pages
-import HomePage from './pages/HomePage';
-import AboutPage from './pages/AboutPage';
-import ProjectsPage from './pages/ProjectsPage';
-import BlogPage from './pages/BlogPage';
-import ContactPage from './pages/ContactPage';
-import MediaGalleryPage from './pages/MediaGalleryPage'; // 1. Import the new page
-import DonatePage from './pages/DonatePage';
-import LoginPage from './pages/LoginPage';
-import DashboardPage from './pages/DashboardPage';
-// We'll add more pages here later
 
 function App() {
   return (
-    <>
-      <Header />
-      <main className="py-3">
-        <Container>
-          <Routes>
-
-            <Route path="/" element={<HomePage />} />
-            <Route path="/about" element={<AboutPage />} />
-            <Route path="/projects" element={<ProjectsPage />} />
-            <Route path="/volunteer" element={<VolunteerPage />} />
-            <Route path="/blog" element={<BlogPage />} />
-            <Route path="/contact" element={<ContactPage />} />
-            <Route path="/media" element={<MediaGalleryPage />} /> 
-            <Route path="/donate" element={<DonatePage />} />
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/dashboard" element={<DashboardPage />} />
-            
-          </Routes>
-        </Container>
-      </main>
-      <Footer />
-    </>
+    // This is the simplest possible component. If this fails, the error is in main.jsx or a global dependency.
+    <Container className="p-5">
+      <h1>Deployment Test Success!</h1>
+      <p>This is a temporary test page.</p>
+    </Container>
   );
 }
 
 export default App;
-import VolunteerPage from './pages/VolunteerPage';

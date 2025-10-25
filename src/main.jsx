ccf966d7812a6097ca66db88d4a4f08957a981d9
Faizan Shaikh <faizan0957@gmail.com>
2025-10-26 03:39:52 +0530

    Apply Bootswatch theme (Solar)

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 // src/main.jsx
-
-import 'bootstrap/dist/css/bootstrap.min.css';
+import './assets/css/bootstrap.min.css'; 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';

3a8abd491a133a56c77a67dc4eeda1e3e9b1ad76
Faizan Shaikh <faizan0957@gmail.com>
2025-10-26 18:08:41 +0530

    Apply Bootswatch theme (Morph)

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 // src/main.jsx
-// import './assets/css/bootstrap.min.css'; 
-import 'bootstrap/dist/css/bootstrap.min.css';
+import './assets/css/bootstrap.min.css'; 
+// import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';

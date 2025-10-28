c42ce4c44fdb9a1cd1749647bb91e398ce0ee507
Faizan Shaikh <faizan0957@gmail.com>
2025-10-29 00:03:46 +0530

    Fix: Increased blog card description length to 300 characters

--- src/pages/BlogPage.jsx
+++ src/pages/BlogPage.jsx
@@ -43,7 +43,7 @@ function BlogPage() {
                 )}
                 <Card.Body>
                   <Card.Title>{post.title}</Card.Title>
-                  <Card.Text>{post.content.substring(0, 150)}...</Card.Text>
+                  <Card.Text>{post.content.substring(0, 450)}...</Card.Text>
                 </Card.Body>
               </Card>
             </Col>
